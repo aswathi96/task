2f92e464c84d4950e2e1afcdafe9112799220eb6
Manu Krishnan T V <tvm@dayscholars.com>
2019-09-03 15:56:02 +0530

    Format Code

--- task2.php
+++ task2.php
@@ -10,48 +10,41 @@
 </head>
 <body >
   <?php
-  $server="localhost";
-  $username="dayscholars";
-  $password="password";
-  $database = "mydatabase";
+$server = "localhost";
+$username = "dayscholars";
+$password = "password";
+$database = "mydatabase";
 
-  /* Create database  connection with correct username and password*/
-  $connect= new mysqli($servername,$username,$password,$database);
+/* Create database  connection with correct username and password*/
+$connect = new mysqli($servername, $username, $password, $database);
 
-  /* Check the connection is created properly or not */
-  if($connect->connect_error)
-
-      echo "Connection error:" .$connect->connect_error;
-      else
-      {
-      }/* echo "Connection is created successfully";*/
-  if(isset($_POST['submit']))
-  {
-  $textarea=$_POST['textarea'];
+/* Check the connection is created properly or not */
+if ($connect->connect_error) {
+    echo "Connection error:" . $connect->connect_error;
+} else {
+} /* echo "Connection is created successfully";*/
+if (isset($_POST['submit'])) {
+    $textarea = $_POST['textarea'];
 
 /*  echo $textarea;*/
 
-
-
-  $sql = "INSERT INTO todo (todotask,date,checked) VALUES ('$textarea', CURRENT_TIME(),0)";
-  $query=mysqli_query($connect,$sql);
+    $sql = "INSERT INTO todo (todotask,date,checked) VALUES ('$textarea', CURRENT_TIME(),0)";
+    $query = mysqli_query($connect, $sql);
 }
 
-
-    /*$sq = "UPDATE todo SET checked=1 where ";
-    $d= mysqli_query($connect, $sq);
-
+/*$sq = "UPDATE todo SET checked=1 where ";
+$d= mysqli_query($connect, $sq);
 
 mysqli_select_db($connect,$database);
-  if (mysqli_query($connect,$sql))
-  {
-      echo "New record created successfully";
-  }
-  else
-  {
-      echo "Error: " . $sql . "<br>" . $connect->error;
-  }
-*/
+if (mysqli_query($connect,$sql))
+{
+echo "New record created successfully";
+}
+else
+{
+echo "Error: " . $sql . "<br>" . $connect->error;
+}
+ */
 
 ?>
 <div class="container">
@@ -89,56 +82,53 @@ mysqli_select_db($connect,$database);
       <table border="1" style="width:100%;">
 
         <?php
-      $result = mysqli_query( $connect,"SELECT id,todotask,date FROM `todo` WHERE checked=0 ORDER BY date DESC");
-      if (mysqli_num_rows($result)>0)
-      {
+$result = mysqli_query($connect, "SELECT id,todotask,date FROM `todo` WHERE checked=0 ORDER BY date DESC");
+if (mysqli_num_rows($result) > 0) {
     // Read the records
-    while($row =mysqli_fetch_array($result))
-     {
-       $id=$row['id'];
-
+    while ($row = mysqli_fetch_array($result)) {
+        $id = $row['id'];
 
 /* echo "<tr >  <td style="width:75%;">" . $row["todotask"]. "</td>";
 
-              <?php
-    $qry2=mysqli_query($conn,"SELECT tname,date,status FROM tbltasks WHERE status=0");
-    if(mysqli_num_rows($qry2)>0)
-    {
-        while($row=mysqli_fetch_array($qry2))
-        {
-  ?>
+<?php
+$qry2=mysqli_query($conn,"SELECT tname,date,status FROM tbltasks WHERE status=0");
+if(mysqli_num_rows($qry2)>0)
+{
+while($row=mysqli_fetch_array($qry2))
+{
+?>
 
-   <li class="list-group-item d-flex justify-content-between align-items-center">
-    <?php echo $row['tname']; ?>
-     <small><?php echo $row['date']; ?></small>
-     <div class="input-group-append">
-     <input type="checkbox" id="chkTodo">
-     </div>
-   </li>
-   <?php }} ?>
-  </ul>
-        */
+<li class="list-group-item d-flex justify-content-between align-items-center">
+<?php echo $row['tname']; ?>
+<small><?php echo $row['date']; ?></small>
+<div class="input-group-append">
+<input type="checkbox" id="chkTodo">
+</div>
+</li>
+<?php }} ?>
+</ul>
+ */
 
-         ?>
+        ?>
 <tr>
-  <td  class="d-none"><input type="hidden" Value="<?php  echo $id ?>" name="hiddenfield"></td>
+  <td  class="d-none"><input type="hidden" Value="<?php echo $id ?>" name="hiddenfield"></td>
 
 
   <td style="width:55%;height:100px;">
     <?php
-    echo "<p>".$row['date']."</p>";
-echo "<p>". $row['todotask']."</p>";
-     ?>
+echo "<p>" . $row['date'] . "</p>";
+        echo "<p>" . $row['todotask'] . "</p>";
+        ?>
 </td>
           <td style="width:25%;" style="align:center;">
             <!-- Button to Open the Modal -->
-<button type="button"  class="btn btn-secondary " style=""><a href="update.php?id=<?php echo $row[id];?>">done</a></button>
+<button type="button"  class="btn btn-secondary " style=""><a href="update.php?id=<?php echo $row[id]; ?>">done</a></button>
 
 </td>
 </tr>
 <?php
 }
-} ?>
+}?>
 </table>
 </form>
 </div>
@@ -147,53 +137,50 @@ echo "<p>". $row['todotask']."</p>";
       <form method="post">
       <table border="1" style="width:100%;">
         <?php
-      $resul = mysqli_query( $connect,"SELECT id,todotask,date FROM `todo` WHERE checked=1 ORDER BY date DESC");
-      if (mysqli_num_rows($resul)>0)
-      {
+$resul = mysqli_query($connect, "SELECT id,todotask,date FROM `todo` WHERE checked=1 ORDER BY date DESC");
+if (mysqli_num_rows($resul) > 0) {
     // Read the records
-    while($ro =mysqli_fetch_array($resul))
-     {
-
+    while ($ro = mysqli_fetch_array($resul)) {
 
 /* echo "<tr >  <td style="width:75%;">" . $row["todotask"]. "</td>";
 
-              <?php
-    $qry2=mysqli_query($conn,"SELECT tname,date,status FROM tbltasks WHERE status=0");
-    if(mysqli_num_rows($qry2)>0)
-    {
-        while($row=mysqli_fetch_array($qry2))
-        {
-  ?>
+<?php
+$qry2=mysqli_query($conn,"SELECT tname,date,status FROM tbltasks WHERE status=0");
+if(mysqli_num_rows($qry2)>0)
+{
+while($row=mysqli_fetch_array($qry2))
+{
+?>
 
-   <li class="list-group-item d-flex justify-content-between align-items-center">
-    <?php echo $row['tname']; ?>
-     <small><?php echo $row['date']; ?></small>
-     <div class="input-group-append">
-     <input type="checkbox" id="chkTodo">
-     </div>
-   </li>
-   <?php }} ?>
-  </ul>
-        */
+<li class="list-group-item d-flex justify-content-between align-items-center">
+<?php echo $row['tname']; ?>
+<small><?php echo $row['date']; ?></small>
+<div class="input-group-append">
+<input type="checkbox" id="chkTodo">
+</div>
+</li>
+<?php }} ?>
+</ul>
+ */
 
-         ?>
+        ?>
 <tr>
-<td  class="d-none"><input type="hidden" Value="<?php  echo $id ?>" name="hiddenfield"></td>
+<td  class="d-none"><input type="hidden" Value="<?php echo $id ?>" name="hiddenfield"></td>
   <td style="width:55%;height:100px;">
     <?php
-    echo "<p>".$ro['date']."</p>";
-echo "<p>". $ro['todotask']."</p>";
-     ?>
+echo "<p>" . $ro['date'] . "</p>";
+        echo "<p>" . $ro['todotask'] . "</p>";
+        ?>
    </td>
 
           <td style="width:25%;">
             <!-- Button to Open the Modal -->
-<button type="button" class="btn btn-warning float-right btn-lg"><a href="updat.php?id=<?php echo $ro[id];?>" ><p color="black">done</p></a></button>
+<button type="button" class="btn btn-warning float-right btn-lg"><a href="updat.php?id=<?php echo $ro[id]; ?>" ><p color="black">done</p></a></button>
 </td>
 </tr>
 <?php
 }
-} ?>
+}?>
 </table>
   </form>
         </div>
